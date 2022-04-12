8d778951fd568aafd4f11c54c83be27e459ed10b
nohjungbin <85227130+nohjungbin@users.noreply.github.com>
2022-04-12 16:13:23 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 function Error_sign () {
-    music.playTone(988, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Whole))
 }
-input.onGesture(Gesture.SixG, function () {
-	
-})
 basic.forever(function () {
-    if (input.rotation(Rotation.Pitch) >= 20) {
-        music.setTempo(1000)
+    if (180 <= input.rotation(Rotation.Roll) && input.rotation(Rotation.Roll) <= 360 || input.rotation(Rotation.Roll) >= -110 && input.rotation(Rotation.Roll) <= -10) {
+        music.setTempo(300)
         Error_sign()
     }
 })
